ffbff9e3e1aa06717bbeb82670300312306dcd85
karsyn ansari <karsyn.ansari@gmail.com>
2021-12-03 11:58:34 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, events) {
+  let newArr = [];
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+    newArr.push(message);
+  }
+  return newArr;
+}
+function countDown(int) {
+  let i = 10;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+  return int;
+}
