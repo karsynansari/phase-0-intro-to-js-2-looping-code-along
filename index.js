194399c63e107aa2412a4eb3b8413e8f42b86c2c
karsyn ansari <karsyn.ansari@gmail.com>
2021-12-15 18:00:26 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function writeCards(names, events) {
-  let newArr = [];
-  for (let i = 0; i < names.length; i++) {
-    let message = `Thank you, ${names[i]}, for the wonderful ${events} gift!`;
-    newArr.push(message);
+// function writeCards(names, events) {
+//   let newArr = [];
+//   for (let i = 0; i < names.length; i++) {
+//     let message = `Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+//     newArr.push(message);
+//   }
+//   return newArr;
+// }
+// function countDown(int) {
+//   let i = 10;
+//   while (i >= 0) {
+//     console.log(i);
+//     i--;
+//   }
+//   return int;
+// }
+
+function writeCards(guestName, event) {
+  let newArray = [];
+  for (let i = 0; i < guestName.length; i++) {
+    let greeting = `Thank you, ${guestName[i]}, for the wonderful ${event} gift!`;
+
+    newArray.push(greeting);
   }
-  return newArr;
+  return newArray;
 }
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
 function countDown(int) {
   let i = 10;
   while (i >= 0) {
@@ -14,3 +34,4 @@ function countDown(int) {
   }
   return int;
 }
+countDown();
